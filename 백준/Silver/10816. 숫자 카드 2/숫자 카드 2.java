70ad3b1090528a2682cf015a7aafb7ee68d1b682
AnMin <100991171+AnMin2000@users.noreply.github.com>
2024-12-30 22:34:21 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 996 ms, Memory: 249896 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,43 +1,36 @@
-import java.util.*;
-import java.io.*;
-class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
-        
-        int m = Integer.parseInt(br.readLine());
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        int[] arr2 = new int[m];
-        for(int i = 0; i < m; i++) arr2[i] = Integer.parseInt(st2.nextToken());
-        
-        Arrays.sort(arr);
-        
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < m; i++){
-            
-            int start = 0;
-            int end = n;
-            
-            while(start < end){
-                int mid = (start + end) / 2;
-                if(arr[mid] < arr2[i]) start = mid + 1;
-                else end = mid;
-            }
-            
-            int start2 = 0;
-            int end2 = n;
-            
-            while(start2 < end2){
-                int mid = (start2 + end2) / 2;
-                if(arr[mid] <= arr2[i]) start2 = mid + 1;
-                else end2 = mid;
-            }
-            sb.append((end2 - start) + " ");
-        }
-        System.out.println(sb.toString());
-    }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		HashMap<Integer, Integer> hash = new HashMap<Integer, Integer>();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			int input = Integer.parseInt(st.nextToken());
+            //해당 key값에 저장된 value가 null이면 1을 대입 아니면 +1을 해준다.
+			if(hash.get(input) == null) hash.put(input, 1);
+			else hash.put(input, hash.get(input) + 1);
+		}
+		
+		
+		int M = Integer.parseInt(br.readLine());
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < M; i++) {
+			int input = Integer.parseInt(st.nextToken());
+            //해당 key값에 저장된 value가 null이면 0을 sb에 저장해준다.
+			if(hash.get(input) == null) sb.append(0).append(" ");
+			else sb.append(hash.get(input)).append(" ");
+		}
+		System.out.println(sb);
+	}
+
 }
