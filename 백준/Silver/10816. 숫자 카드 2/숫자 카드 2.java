31488c86203e0f50a0d5c2fd9ce5ab38ca8e3da0
AnMin <100991171+AnMin2000@users.noreply.github.com>
2024-12-30 22:36:58 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 744 ms, Memory: 254444 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,25 +9,21 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		HashMap<Integer, Integer> hash = new HashMap<Integer, Integer>();
+		int[] arr = new int[20000001];
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) {
 			int input = Integer.parseInt(st.nextToken());
-            //해당 key값에 저장된 value가 null이면 1을 대입 아니면 +1을 해준다.
-			if(hash.get(input) == null) hash.put(input, 1);
-			else hash.put(input, hash.get(input) + 1);
+			//해당 인덱스 값에 +1해준다.
+			arr[input + 10000000]++;
 		}
-		
 		
 		int M = Integer.parseInt(br.readLine());
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < M; i++) {
 			int input = Integer.parseInt(st.nextToken());
-            //해당 key값에 저장된 value가 null이면 0을 sb에 저장해준다.
-			if(hash.get(input) == null) sb.append(0).append(" ");
-			else sb.append(hash.get(input)).append(" ");
+			sb.append(arr[input + 10000000]).append(" ");
 		}
 		System.out.println(sb);
 	}
