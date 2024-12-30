9af3f842c315638fce65beae9913abe56ca9ca31
AnMin <100991171+AnMin2000@users.noreply.github.com>
2024-12-30 15:25:58 +0900

    [Gold IV] Title: 오큰수, Time: 1056 ms, Memory: 320092 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        
+        int[] arr = new int[n];
+        int[] result = new int[n];
+        
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        
+        String[] tmp = br.readLine().split(" ");
+        
+        for(int i = 0; i < n; i++) arr[i] = Integer.valueOf(tmp[i]);
+        result[n - 1] = -1;
+        stack.push(arr[n - 1]);
+
+        for(int i = n - 2; i >= 0; i--){
+
+        	result[i] = -1;
+            if(arr[i] < arr[i + 1]){
+                result[i] = arr[i + 1];
+                stack.push(arr[i]);
+            }
+            else{
+                while(!stack.isEmpty()){
+                    if(arr[i] < stack.peek()){
+                        result[i] = stack.peek();
+                        stack.push(arr[i]);
+                        break;
+                    }
+                    stack.pop();
+                }
+                if(result[i] == -1) stack.push(arr[i]);
+            }
+        }
+        for(int i = 0; i < n; i++) sb.append(result[i] + " ");
+        System.out.println(sb.toString());
+    }
+}
