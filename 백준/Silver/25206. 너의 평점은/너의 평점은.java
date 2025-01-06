ae9368349f249473adfcf0281de47b8fea9d762f
AnMin <100991171+AnMin2000@users.noreply.github.com>
2025-01-06 13:19:09 +0900

    [Silver V] Title: 너의 평점은, Time: 72 ms, Memory: 11904 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        double sum = 0;
+        
+        
+        double level = 0;
+        
+        for(int i = 0; i < 20; i++){
+            String[] arr = br.readLine().split(" ");
+            
+            if(arr[2].equals("P")) continue;
+            
+            long tmp = arr[1].charAt(0) - '0';
+            sum += tmp;
+            //System.out.println(tmp);
+            if(arr[2].equals("A+")) level += (4.5 * tmp) ;
+            else if(arr[2].equals("A0")) level += (4 * tmp);
+            else if(arr[2].equals("B+")) level += (3.5 * tmp);
+            else if(arr[2].equals("B0")) level += (3 * tmp);
+            else if(arr[2].equals("C+")) level += (2.5 * tmp);
+            else if(arr[2].equals("C0")) level += (2 * tmp);
+            else if(arr[2].equals("D+")) level += (1.5 * tmp);
+            else if(arr[2].equals("D0")) level += (1 * tmp);
+            else if(arr[2].equals("F")) level += 0;
+        }
+        
+        System.out.printf("%8f", level / sum);
+     
+        
+        
+    }
+}
