21cc614f917b946cb07dcf26110d8af644a387fe
AnMin <100991171+AnMin2000@users.noreply.github.com>
2025-01-06 14:59:32 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 120 ms, Memory: 16188 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -1,39 +1,31 @@
-import java.io.*;
 import java.util.*;
-class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String s = br.readLine();
-        
-        Stack<Character> stack = new Stack<>();
-        
-        StringBuilder sb = new StringBuilder();
-        
-        boolean state = true;
-        for(int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
-            
-            if(ch == '<' || ch  == ' ') {
-            	while(!stack.isEmpty())	sb.append(stack.pop());
-            }
-            
-            if(ch == '<') state = false;
-            
-            if(state){
-                if(ch == ' ') sb.append(' ');
-                else stack.push(ch);
-            } 
-            else if(!state) sb.append(ch);
-            
-            if(ch == '>') state = true;
-            
-        }
-        
-        while(!stack.isEmpty()){
-            sb.append(stack.pop());
-        }
-        
-        System.out.println(sb.toString());
-    }
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," <>", true);
+		
+
+		StringBuilder sb = new StringBuilder();
+		
+		while(st.hasMoreTokens()) {
+			String s = st.nextToken();
+			if(s.equals("<")) {
+				while(!(s.equals(">"))) {
+					sb.append(s);
+					s = st.nextToken();
+				}
+				sb.append(s);
+			}
+			else {
+				for(int i=s.length()-1; i>=0; i--) {
+					sb.append(s.charAt(i));
+				}
+			}
+		}		
+		System.out.println(sb.toString());
+	}
+
 }
