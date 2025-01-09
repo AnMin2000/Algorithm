5c6f2de8b8aa232d5d85ef88769e056535ac9604
AnMin <100991171+AnMin2000@users.noreply.github.com>
2025-01-09 14:02:05 +0900

    [Silver IV] Title: 암기왕, Time: 1292 ms, Memory: 334940 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -1,25 +1,38 @@
 import java.util.*;
 import java.io.*;
-class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        
-        StringBuilder sb = new StringBuilder();
-        while(T-->0){
-            int N = Integer.parseInt(br.readLine());
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int[] arr = new int[N];
-            for(int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
-            Arrays.sort(arr);
-            int M = Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine());
-            for(int i = 0; i < M; i++){
-                if(Arrays.binarySearch(arr, Integer.parseInt(st.nextToken())) > -1) sb.append("1 \n");
-                else sb.append("0 \n");
-            }
-            
-        }
-        System.out.println(sb.toString());
-    }
+ 
+class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	static int T, N, M;
+	static int[] arr;
+	
+	public static void main(String[] args) throws IOException {
+		T = Integer.parseInt(br.readLine());
+		
+		for (int t = 0; t < T; t++) {
+			N = Integer.parseInt(br.readLine());
+			arr = new int[N];
+			
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			Arrays.sort(arr);
+			M = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < M; i++) {
+				int check = Integer.parseInt(st.nextToken());
+				if (Arrays.binarySearch(arr, check) >= 0) {
+					sb.append(1).append("\n");
+				}else {
+					sb.append(0).append("\n");
+				}
+			}
+			
+		}
+		System.out.println(sb);
+	}
 }
