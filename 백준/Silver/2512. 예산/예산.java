4ad13424b378ef519097821fa19ebb02ba22a49f
AnMin <100991171+AnMin2000@users.noreply.github.com>
2025-01-10 13:25:37 +0900

    [Silver II] Title: 예산, Time: 108 ms, Memory: 14040 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		int max = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			max = Math.max(max, arr[i]);
+		}
+		int m = Integer.parseInt(br.readLine());
+		
+		Arrays.sort(arr);
+		
+		int start = 0;
+		int end = max;
+		int mid = 0;
+		int result = 0;
+		while(start <= end) {
+			mid = (start + end) / 2;
+			int sum = 0;
+			for(int i = 0; i < n; i++) {
+				if(arr[i] > mid) {
+					sum += mid;
+				}
+				else sum += arr[i];
+			}
+			
+			if(sum <= m) {
+				result = mid;
+				start = mid + 1;
+			}
+			else {
+				end = mid - 1;
+			}
+
+		}
+		
+		System.out.println(result);
+	}
+}
