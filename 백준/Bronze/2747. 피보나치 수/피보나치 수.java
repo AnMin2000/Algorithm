9e0ba02b9cd895949a213c06b3860bfa01182502
AnMin <100991171+AnMin2000@users.noreply.github.com>
2025-01-12 15:59:59 +0900

    [Bronze II] Title: 피보나치 수, Time: 64 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		long[] dp = new long[N + 1];
+		
+		if(N == 0) {
+			System.out.println(0);
+			return;
+		}
+		else if(N <= 2) {
+			System.out.println(1);
+			return;
+		}
+		dp[2] = dp[1] = 1;
+		for(int i = 3; i <= N; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+		System.out.print(dp[N]);
+	}
+}
