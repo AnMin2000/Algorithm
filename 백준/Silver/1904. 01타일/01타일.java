ee85ced2b79226f0343c57b532cb69cf78769179
AnMin <100991171+AnMin2000@users.noreply.github.com>
2025-01-12 17:08:45 +0900

    [Silver III] Title: 01타일, Time: 88 ms, Memory: 19348 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+
+		long[] dp = new long[n + 1];
+		if(n <= 2) {
+			System.out.println(n);
+			return;
+		}
+		dp[1] = 1;
+		dp[2] = 2;
+		for(int i = 3; i <= n; i++) dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+		
+		System.out.println(dp[n]);
+		
+		
+	}
+}
