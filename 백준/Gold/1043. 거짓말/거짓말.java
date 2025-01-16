3e6b9a75a227eba8bf5ec3d435b1af71f85df151
AnMin <100991171+AnMin2000@users.noreply.github.com>
2025-01-16 22:23:10 +0900

    [Gold IV] Title: 거짓말, Time: 72 ms, Memory: 11684 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -1,62 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
-class Main{
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken()); // 사람수
-		int m = Integer.parseInt(st.nextToken()); // 파티수
-		
-		Set<Integer> set = new LinkedHashSet<>();
-		st = new StringTokenizer(br.readLine());
-		int tmp = Integer.parseInt(st.nextToken());
-		while(tmp-->0) {
-			set.add(Integer.parseInt(st.nextToken()));
-		}
+public class Main {
 
-		List<Integer>[] list = new ArrayList[m + 1];
-		for(int i = 1; i <= m; i++) list[i] = new ArrayList<>();
-		
-		for(int j = 1; j <= m; j++) {
-			st = new StringTokenizer(br.readLine());
-			int count = Integer.parseInt(st.nextToken());
-			for(int i = 0; i < count; i++) {
-				list[j].add(Integer.parseInt(st.nextToken()));
-			}
-		}
-		
-		boolean update = true;
-		while(update) {
-			update = false;
-			for(int i = 1; i <= m; i++) {
-				for(int s : list[i]) {
-					if(set.contains(s)) {
-						for(int ss : list[i]) {
-							if(!set.contains(ss)) {
-								set.add(ss);
-								update = true;
-							}
-						}
-						break;
-					}
-				}
-			}
-		}
-		
-		
-		int ctn = 0;
-		for(int i = 1; i <= m; i++) {
-			boolean check = true;
-			for(int s : list[i]) {
-				if(set.contains(s)) {
-					check = false;
-					break;
-				}
-			}
-			if(check) ctn++;
-		}
-		System.out.println(ctn);
-	}
+    static int[] trueMan;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+
+        input = br.readLine().split(" ");
+        int inputLen = input.length;
+
+        if(inputLen == 1) {
+            System.out.println(M);
+            return;
+        }
+
+        List<String> trueMan = new ArrayList<>();
+        for(int i = 1; i < inputLen; i ++) {
+            trueMan.add(input[i]);
+        }
+
+        List<int[]> tmp = new ArrayList<>();
+        for(int i = 0; i < M; i ++) {
+            input = br.readLine().split(" ");
+            inputLen = input.length;
+
+            int[] arr = new int[N + 1];
+            for (int j = 1; j < inputLen; j ++) {
+                arr[Integer.parseInt(input[j])]++;
+            }
+            tmp.add(arr);
+        }
+        
+        Queue<Integer> queue = new LinkedList<>();
+        for(String s : trueMan) {
+            queue.add(Integer.parseInt(s));
+        }
+
+        int answer = M;
+        boolean[] visited = new boolean[N + 1];
+        boolean[] visitedM = new boolean[M];
+
+        while(!queue.isEmpty()) {
+            int now = queue.poll();
+            visited[now] = true;
+
+            for(int i = 0; i < M; i ++) {
+                int[] tmpArr = tmp.get(i);
+                if(tmpArr[now] > 0 && !visitedM[i]) {
+                    for(int j = 1; j < tmpArr.length; j ++) {
+                        if(tmpArr[j] == 1 && !visited[j]) {
+                            queue.add(j);
+                            visited[j] = true;
+                        }
+                    }
+                    answer--;
+                    visitedM[i] = true;
+                    if(answer == 0) {
+                        System.out.println(answer);
+                        return;
+                    }
+                }
+            }
+        }
+        System.out.println(answer);
+    }
 }
