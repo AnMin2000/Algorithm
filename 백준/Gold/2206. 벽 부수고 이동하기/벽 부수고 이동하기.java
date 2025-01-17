3d7e70e649f485ad67a5909009ea9a5db71f4d6a
AnMin <100991171+AnMin2000@users.noreply.github.com>
2025-01-17 23:05:05 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 536 ms, Memory: 146912 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,68 +1,58 @@
 import java.util.*;
 import java.io.*;
 class Main{
-    
-    static int y, x;
-    static char[][] cube;
-    static boolean[][][] visit;
-    static Queue<int[]> queue = new LinkedList<>();
-    static int result = -1;
-    
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        y = Integer.parseInt(st.nextToken());
-        x = Integer.parseInt(st.nextToken());
-        
-        visit = new boolean[y][x][2];
-        cube = new char[y][x];
-        
-        for(int j = 0; j < y; j++){  
-            cube[j] = br.readLine().toCharArray();
-        }
-        
-        queue.offer(new int[]{0, 0, 1, 0});
-        bfs();
-        System.out.println(result);
-    }
-    static void bfs(){
-        int[][] xy = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-        
-        visit[0][0][0] = true;
-        while(!queue.isEmpty()){
-            int[] arr = queue.poll();
-            int y1 = arr[0];
-            int x1 = arr[1];
-            int ctn = arr[2];
-            int check = arr[3];
-            
-            
-            if(y1 == y - 1 && x1 == x - 1){
-            	result = ctn;
-                return;
-            }
-            
-            for(int i = 0; i < 4; i++){
-                int y2 = y1 + xy[i][0];
-                int x2 = x1 + xy[i][1];
-                
-                
-                if(y2 >= 0 && x2 >= 0 && y2 < y && x2 < x) {
-                	if(cube[y2][x2] == '0' && !visit[y2][x2][check]){
-                        visit[y2][x2][check] = true;
-                        queue.offer(new int[] {y2, x2, ctn + 1, check});
-                    }
-                    else if(cube[y2][x2] == '1' && check == 0 && !visit[y2][x2][1]){
-                        visit[y2][x2][1] = true;
-                        queue.offer(new int[] {y2, x2, ctn + 1, 1});
-                    }
-                }
-                
-                
-                
-            }
-        }
-    }
+	static int n, m;
+	static char[][] arr;
+	static boolean[][][] visit;
+
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new char[n][m];
+		visit = new boolean[n][m][2];
+		
+		for(int j = 0; j < n; j++) {
+			arr[j] = br.readLine().toCharArray();
+		}
+
+		System.out.println(bfs());
+	}
+	static int bfs() {
+		Queue<int[]> queue = new LinkedList<>();
+		queue.offer(new int[] {0, 0, 1, 0});
+		int[][] xy = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+		visit[0][0][0] = true;
+		while(!queue.isEmpty()) {
+			int[] ar = queue.poll();
+			int y = ar[0];
+			int x = ar[1];
+			int ctn = ar[2];
+			int wall = ar[3];
+			
+			if(y == n - 1 && x == m - 1) return ctn;
+			
+			for(int i = 0; i < 4; i++) {
+				int ny = y + xy[i][0];
+				int nx = x + xy[i][1];
+				
+				if(ny < 0 || nx < 0 || nx >= m || ny >= n ) continue;
+				
+				if(arr[ny][nx] - '0' == 0 && !visit[ny][nx][wall]) {
+					visit[ny][nx][wall] = true;
+					queue.offer(new int[] {ny, nx, ctn + 1, wall});
+					//System.out.println("Y : " + ny + " X : " + nx + " Count : " + (ctn + 1) + " Wall : " + wall);
+				}
+				else if(arr[ny][nx] - '0' == 1 && !visit[ny][nx][1] && wall == 0) {
+					visit[ny][nx][1] = true;
+					queue.offer(new int[] {ny, nx, ctn + 1, 1});
+					//System.out.println("Y : " + ny + " X : " + nx + " Count : " + (ctn + 1) + " Wall : 1");
+				}
+				
+			}
+		}
+		return -1;
+	}
 }
