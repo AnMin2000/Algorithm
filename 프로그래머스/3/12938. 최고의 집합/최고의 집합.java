71cac12b287a0faeb2b9e7b154f9ecf8d89572b1
AnMin <100991171+AnMin2000@users.noreply.github.com>
2025-01-20 18:56:25 +0900

    [level 3] Title: 최고의 집합, Time: 0.25 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -1,6 +1,19 @@
+import java.util.*;
 class Solution {
     public int[] solution(int n, int s) {
-        int[] answer = {};
+        if(s / n == 0) return new int[]{-1};
+        
+        int n1 = s / n;
+        int n2 = s % n;
+        
+        int[] answer = new int[n];
+        Arrays.fill(answer, n1);
+        
+        int i = n - 1;
+        while(n2-->0){
+            if(i < 0) i = n - 1;
+            answer[i--]++;
+        }
         return answer;
     }
 }
