d74959e16b8d05ad239b73254cfcf09d47cd8c53
AnMin <100991171+AnMin2000@users.noreply.github.com>
2025-01-27 15:21:32 +0900

    [Silver V] Title: 집합, Time: 1748 ms, Memory: 327944 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -6,28 +6,28 @@ class Main{
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		Set<String> set = new LinkedHashSet<>();
+		Set<Integer> set = new LinkedHashSet<>();
 		StringBuilder sb = new StringBuilder();
 		while(n-->0) {
 			String[] arr = br.readLine().split(" ");
 			
 			if(arr[0].equals("add")) {
-				set.add(arr[1]);
+				set.add(Integer.parseInt(arr[1]));
 			}
 			else if(arr[0].equals("remove")) {
-				if(set.contains(arr[1])) set.remove(arr[1]);
+				if(set.contains(Integer.parseInt(arr[1]))) set.remove(Integer.parseInt(arr[1]));
 			}
 			else if(arr[0].equals("check")) {
-				if(set.contains(arr[1])) sb.append(1).append("\n");
+				if(set.contains(Integer.parseInt(arr[1]))) sb.append(1).append("\n");
 				else sb.append(0).append("\n");
 			}
 			else if(arr[0].equals("toggle")) {
-				if(set.contains(arr[1])) set.remove(arr[1]);
-				else set.add(arr[1]);
+				if(set.contains(Integer.parseInt(arr[1]))) set.remove(Integer.parseInt(arr[1]));
+				else set.add(Integer.parseInt(arr[1]));
 			}
 			else if(arr[0].equals("all")) {
 				set.clear();
-				for(int i = 1; i <= 20; i++) set.add(String.valueOf(i));
+				for(int i = 1; i <= 20; i++) set.add(i);
 			}
 			else if(arr[0].equals("empty")) {
 				set.clear();
